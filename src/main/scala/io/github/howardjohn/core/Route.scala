2a05144f7b099068ebca79c83e49c2f915f635e8
John <johnbhoward96@gmail.com>
2018-02-05 19:06:41 -0800

    Add namespace

--- src/main/scala/io/github/howardjohn/core/Route.scala
+++ src/main/scala/io/github/howardjohn/core/Route.scala
@@ -13,32 +13,31 @@ import io.circe.generic.auto._
 class Route[T](dbCreator: String => ConfigDatastore)(
   implicit encoders: Encoder[Seq[ConfigEntry]],
   encoder: Encoder[ConfigEntry]) {
-  val db = dbCreator("example")
   implicit val entryDecoder = jsonOf[IO, ConfigEntry]
   implicit val jsonDecoder = jsonOf[IO, Json]
 
   val service: HttpService[IO] = HttpService[IO] {
-    case GET -> Root => db.getAll().flatMap(entries => Ok(entries.asJson))
-    case GET -> Root / key =>
-      db.get(key).flatMap {
+    case GET -> Root / namespace => dbCreator(namespace).getAll().flatMap(entries => Ok(entries.asJson))
+    case GET -> Root / namespace / key =>
+      dbCreator(namespace).get(key).flatMap {
         case Some(v) => Ok(v.asJson)
         case None => NotFound()
       }
-    case DELETE -> Root / key =>
+    case DELETE -> Root / namespace / key =>
       for {
-        _ <- db.delete(key)
+        _ <- dbCreator(namespace).delete(key)
         resp <- Ok("")
       } yield resp
-    case req @ PUT -> Root / key =>
+    case req @ PUT -> Root / namespace / key =>
       for {
         entry <- req.as[Json]
-        _ <- db.update(key, entry)
+        _ <- dbCreator(namespace).update(key, entry)
         response <- Ok("")
       } yield response
-    case req @ POST -> Root =>
+    case req @ POST -> Root / namespace =>
       for {
         entry <- req.as[ConfigEntry]
-        _ <- db.write(entry.key, entry.value)
+        _ <- dbCreator(namespace).write(entry.key, entry.value)
         location <- IO.fromEither(Uri.fromString(s"/${entry.key}"))
         response <- Ok("", Location(location))
       } yield response
