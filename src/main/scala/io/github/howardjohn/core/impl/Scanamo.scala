1adc98b0c371702c8f8930f0373b376427cb1d3f
John <johnbhoward96@gmail.com>
2018-02-28 18:49:21 -0800

    Properly serialze JSON

--- src/main/scala/io/github/howardjohn/core/impl/Scanamo.scala
+++ src/main/scala/io/github/howardjohn/core/impl/Scanamo.scala
@@ -2,19 +2,21 @@ package io.github.howardjohn.core.impl
 
 import cats.Traverse
 import cats.effect.IO
+import cats.implicits._
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsync
-import com.amazonaws.services.dynamodbv2.model.ConditionalCheckFailedException
+import com.amazonaws.services.dynamodbv2.document.Item
+import com.amazonaws.services.dynamodbv2.document.internal.InternalUtils
+import com.amazonaws.services.dynamodbv2.model.{AttributeValue, ConditionalCheckFailedException}
 import com.gu.scanamo.error.{DynamoReadError, TypeCoercionError}
 import com.gu.scanamo.ops.ScanamoOps
-import com.gu.scanamo.query.Query
-import com.gu.scanamo.{DynamoFormat, ScanamoAsync, SecondaryIndex, Table}
+import com.gu.scanamo.{DynamoFormat, ScanamoAsync, Table}
 import io.circe.Json
 import io.circe.parser.parse
 import io.github.howardjohn.core.ConfigError._
 import io.github.howardjohn.core._
 
+import scala.collection.JavaConverters._
 import scala.concurrent.ExecutionContext
-import cats.implicits._
 
 class Scanamo(val dynamo: AmazonDynamoDBAsync)(implicit ec: ExecutionContext) {
   def exec[A](ops: ScanamoOps[A]): IO[A] = IO.fromFuture(IO(ScanamoAsync.exec(dynamo)(ops)))
@@ -35,11 +37,23 @@ object Scanamo {
 
   def configTable(namespace: String): Table[ConfigEntry] = Table[ConfigEntry](namespace)
 
-  implicit val jsonFormat: DynamoFormat[Json] = DynamoFormat.xmap[Json, String](
-    in => parse(in).left.map(f => TypeCoercionError(f))
-  )(
-    json => json.noSpaces
-  )
+  implicit val jsonFormat: DynamoFormat[Json] = new DynamoFormat[Json] {
+    private val placeholder = "document"
+
+    def read(av: AttributeValue): Either[DynamoReadError, Json] =
+      parse {
+        InternalUtils
+          .toItemList(List(Map(placeholder -> av).asJava).asJava)
+          .asScala
+          .head
+          .getJSON(placeholder)
+      }.left.map(f => TypeCoercionError(f))
+
+    def write(json: Json): AttributeValue = {
+      val item = new Item().withJSON(placeholder, json.noSpaces)
+      InternalUtils.toAttributeValues(item).get(placeholder)
+    }
+  }
 
   def mapErrors[A, E](result: Either[E, A]): Either[ConfigError, A] = result.left.map {
     case _: ConditionalCheckFailedException => IllegalWrite
