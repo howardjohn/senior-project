629876099191727107730e35f4b8e32d0327c9b8
John <johnbhoward96@gmail.com>
2018-03-25 10:21:01 -0700

    Removed extra code in client

--- client/shared/src/main/scala/io/github/howardjohn/client/App.scala
+++
@@ -1,9 +0,0 @@
-package io.github.howardjohn.client
-
-trait ConfigDatastore {
-  def getNamespace(namespace: String): ConfigNamespace
-  def getTag(tag: String): ConfigTag
-
-  def createNamespace(namespace: String): Result[ConfigNamespace]
-  def createTag(tag: String, namespace: String): Result[ConfigTag]
-}
